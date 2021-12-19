f28aa003da065962429f417c972d3fe9400efea1
Ksheerja05 <91480917+Ksheerja05@users.noreply.github.com>
2021-12-19 10:24:51 +0530

    Add files via upload

---
+++ models/train_classifier.py
@@ -0,0 +1,108 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sqlite3
+import sqlalchemy
+from sqlalchemy import create_engine
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+nltk.download(['punkt', 'wordnet'])
+
+
+def load_data(database_filepath):
+    
+    # load data from database
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('disaster_messages', engine)
+    
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    return X, Y
+
+
+def tokenize(text):
+    
+    # tokenize text
+    tokens = word_tokenize(text)         
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()     
+    
+    # iterate through tokens
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()     # lemmatize, normalize, and remove white spaces
+        clean_tokens.append(clean_tok)
+    return clean_tokens
+ 
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+    'clf__estimator__n_estimators' : [50, 100, 200]
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test):
+    
+    Y_pred = model.predict(X_test)  
+    
+    for index, column in enumerate(Y_test):
+        print(column, classification_report(Y_test[column], Y_pred[:, index]))
+
+
+def save_model(model, model_filepath):
+    
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
